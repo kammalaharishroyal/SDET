5af9d0d92fd8f909df76f6ef6e6db4a8068ebfc8
harish <kammalaharish3304@gmail.com>
2025-07-02 21:35:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let navbar=document.getElementById("CreateLoc");
+console.log(navbar);
+
+function Display(){
+    console.log("cliked");
+    
+let card=document.getElementsByClassName("CreateLocCard")[0];
+card.style.display="block";
+}
